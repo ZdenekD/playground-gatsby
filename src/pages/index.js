c22aabc13d5ef2c877e9c192a98898e98c8fda54
zdenekd <ditezdenek@gmail.com>
2021-04-12 12:07:42 +0200

    Add blog post generate pages task

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,42 @@
 import React from 'react';
-import {graphql} from 'gatsby';
+import {graphql, Link} from 'gatsby';
+import styled from 'styled-components';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
-const IndexPage = ({data}) => {
-    console.log(data.allMarkdownRemark.edges);
+const BlogLink = styled(Link)`
+    text-decoration: none;
+`;
+const BlogTitle = styled.h3`
+    margin-bottom: 20px;
+    color: blue;
+`;
 
-    return (
-        <Layout>
-            <Seo title="Home" />
-            <div>
-                <h1>Hi people</h1>
+const IndexPage = ({data}) => (
+    <Layout>
+        <Seo title="Home" />
+        <div>
+            <h1>Hi people</h1>
+        </div>
+        <h2>{data.allMarkdownRemark.totalCount}</h2>
+        {data.allMarkdownRemark.edges.map(({node}) => (
+            <div key={node.id}>
+                <BlogLink to={node.fields.slug}>
+                    <BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+                </BlogLink>
+                <p>
+                    {node.excerpt}
+                </p>
             </div>
-            <h2>{data.allMarkdownRemark.totalCount}</h2>
-            {data.allMarkdownRemark.edges.map(({node}) => (
-                <div key={node.id}>
-                    <span>{node.frontmatter.title} - {node.frontmatter.date}</span>
-                    <p>
-                        {node.excerpt}
-                    </p>
-                </div>
-            ))}
-        </Layout>
-    );
-};
+        ))}
+    </Layout>
+);
 
 export default IndexPage;
 
 export const query = graphql`
     query {
-        allMarkdownRemark {
+        allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
             totalCount
             edges {
                 node {
@@ -38,6 +45,9 @@ export const query = graphql`
                         date
                         description
                         title
+                    }
+                    fields {
+                        slug
                     }
                     html
                     excerpt
