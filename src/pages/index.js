48d1481b8ab18de03d410e7afc3ff17ce305e552
zdenekd <ditezdenek@gmail.com>
2021-04-12 11:21:38 +0200

    Updated layout after added eslint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,48 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react';
+import {graphql} from 'gatsby';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+const IndexPage = ({data}) => {
+    console.log(data.allMarkdownRemark.edges);
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+    return (
+        <Layout>
+            <Seo title="Home" />
+            <div>
+                <h1>Hi people</h1>
+            </div>
+            <h2>{data.allMarkdownRemark.totalCount}</h2>
+            {data.allMarkdownRemark.edges.map(({node}) => (
+                <div key={node.id}>
+                    <span>{node.frontmatter.title} - {node.frontmatter.date}</span>
+                    <p>
+                        {node.excerpt}
+                    </p>
+                </div>
+            ))}
+        </Layout>
+    );
+};
 
-export default IndexPage
+export default IndexPage;
+
+export const query = graphql`
+    query {
+        allMarkdownRemark {
+            totalCount
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        date
+                        description
+                        title
+                    }
+                    html
+                    excerpt
+                }
+            }
+        }
+    }
+`;
